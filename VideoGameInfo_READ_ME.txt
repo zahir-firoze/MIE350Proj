a70ad96f705f51c102607bd31d49cd4a19aca0fa
cielfe <43140030+cielfe@users.noreply.github.com>
2019-11-11 23:46:09 -0500

    edited Accessory DAO, data, and controller classes; added test jsp file and created a new database with smaller UPC numbers

--- VideoGameInfo_READ_ME.txt
+++ VideoGameInfo_READ_ME.txt
@@ -4,3 +4,5 @@ please put this line for each day of changes
 10/18/2019,Ciel, created basic structure of object classes  and List classes under videoGame.model package; created the eclipse project;
 =======================================================================================================================================
 10/31/2019,Ciel, created new dynamic web project in eclipse for this project; All back end code is in Java resources; images for front end is in web content; created and updated the web.xml accordingly with the servlet information for this project;
+=======================================================================================================================================
+11/11/2019,Ciel, added filter functionality in the accessories; made a new database called "GameCheck.mdb" and put it in the db.properties; modified AccessoryController, Accessory and AccessoryDao; added a test jsp called "TEST_displayFilterResults"; copied all contents from the lib folder in sample web app into videoGameWebApp
--- videoGameWebApp/src/com/vgi/dao/AccessoryDao.java
+++ videoGameWebApp/src/com/vgi/dao/AccessoryDao.java
@@ -1,11 +1,98 @@
 package com.vgi.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.vgi.model.Accessory;
+import com.vgi.util.DbUtil;
+
 public class AccessoryDao {
-	//TODO
-//	public List<Accessory> getFilteredAccessories(Map selectedAttributes){
-//		//map can be of dynamic size
-//		//search in map which filters are to be applied for the SQL query
-//		return;
+	/**
+	 * This class handles all of the Accessory-related methods
+	 * (filter/sort/get).
+	 */
+//	public static void main(String[] args){
+//		AccessoryDao test = new AccessoryDao();
+//		HashMap<String,Object> input = new HashMap<String,Object>();
+//		
+//		input.put("Price",15.99);
+//		input.put("ConsoleCompatibility","Nintendo3DS");
+//		input.put("Description","Hi Ciel");
+//		System.out.println(test.getFilteredAccessories(input));
 //	}
-// 
+	private Connection connection;
+	
+	public AccessoryDao() {
+		/**
+		 * Get the database connection.
+		 */
+		connection = DbUtil.getConnection();
+	}
+
+	
+	public List<Accessory> getFilteredAccessories(HashMap<String,Object> selectedAttributes){
+		List<Accessory> accessoriesList = new ArrayList<Accessory>();
+		String sqlQueryStatement = "SELECT * FROM Accessories";
+		String columnName;
+		Object value;
+		int counter = 1; //leave this at 1!!
+		int sizeOfFilter = 0;
+		try{
+			Statement statement = connection.createStatement();
+			
+			
+			//map can be of dynamic size
+			
+			if (!selectedAttributes.isEmpty()){
+				//search in map which filters are to be applied for the SQL query
+				//this for each loop creates the SQL Query
+				sqlQueryStatement = sqlQueryStatement + " WHERE";
+				sizeOfFilter = selectedAttributes.keySet().size();
+				for (Object f : selectedAttributes.keySet()){
+					//System.out.println(selectedAttributes.get(f).getClass());
+					columnName = (String) f;
+					value = selectedAttributes.get(f) ;
+					sqlQueryStatement = sqlQueryStatement + " " + columnName;
+					//format the query based on the object in the hashmap
+					if (selectedAttributes.get(f) instanceof java.lang.String){
+						sqlQueryStatement = sqlQueryStatement + "='" + value+"'";
+					}
+					else{
+						sqlQueryStatement = sqlQueryStatement + "=" + value;
+					}
+					if (counter < sizeOfFilter){
+						sqlQueryStatement = sqlQueryStatement + " AND";
+					}
+					counter++;
+					//SELECT * FROM Accessories WHERE ConsoleCompatibility = '3DS' AND Price < 16;
+				}
+			}
+			System.out.println("Query : " + sqlQueryStatement);
+			//execute the dynamically created query
+			ResultSet rs = statement.executeQuery(sqlQueryStatement);
+			//save the list of found accessories in the list
+			while (rs.next()) {
+				Accessory accessory = new Accessory();
+				accessory.setUPCNumber(rs.getInt("UPC"));
+				accessory.setName(rs.getString("Name"));
+				accessory.setConsoleCompatability(rs.getString("ConsoleCompatibility"));
+				accessory.setPrice(rs.getDouble("Price"));
+				accessory.setDescription(rs.getString("Description"));
+				accessoriesList.add(accessory);
+			}
+			
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+		return accessoriesList;
+	}
+ 
 }
